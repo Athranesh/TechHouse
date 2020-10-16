684df8aa5b5f702d884acd4847bd21b96547893c
Athranesh <athranesh@gmail.com>
2020-10-16 20:12:41 +0300

    Added routing. Added backend server. Frontend now gets its data from the backend.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,22 +3,31 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
+  //to be changed when admin panel is added
+  const items = 2;
+
   return (
     <header>
-      <Navbar className="navbar-dark bg-primary " expand="sm">
-        <Container className="align-items-center flex-column flex-sm-row justify-content-center justify-content-sm-between">
-          <LinkContainer to="/">
-            <Navbar.Brand className="mx-3 mx-sm-0">Tech House</Navbar.Brand>
+      <Navbar className="navbar-dark bg-primary" expand="sm">
+        <Container className="align-items-center flex-column flex-sm-row justify-content-sm-between">
+          <LinkContainer className="align-self-start" to="/">
+            <Navbar.Brand className="">Tech House</Navbar.Brand>
           </LinkContainer>
 
-          <Nav className="flex-row">
+          <Nav
+            className="flex-row  
+          align-self-start
+          justify-content-between
+          justify-content-sm-end mt-2 mt-sm-0"
+          >
             <LinkContainer to="/cart">
-              <Nav.Link className="px-2 px-sm-3">
+              <Nav.Link className="mr-4 mr-sm-0">
                 <i className="fas fa-shopping-cart"></i> Cart
               </Nav.Link>
             </LinkContainer>
+
             <LinkContainer to="login">
-              <Nav.Link className="px-2 px-sm-0">
+              <Nav.Link className="mr-4 mr-sm-0">
                 <i className="fas fa-user"></i> Sign In
               </Nav.Link>
             </LinkContainer>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,85 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import axios from 'axios';
 
-function ProductScreen() {
-  return <div className="">Product</div>;
+function ProductScreen({ match }) {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const res = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(res.data);
+    };
+
+    getProduct();
+  }, [match.params.id]);
+
+  if (!product) return null;
+
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col lg={6}>
+          <Image src={product.image} alt={product.name} fluid />
+          {/*fluid stops items from going out of their containers*/}
+        </Col>
+        <Col lg={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col lg={3}>
+          <Card>
+            <ListGroup variant="flish">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock <= 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default ProductScreen;
